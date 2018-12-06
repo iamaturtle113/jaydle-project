8216a71c6c41555eb2ac5282153c77068bef5987
iamaturtle113 <soak2soak@gmail.com>
2018-12-06 13:49:38 +0900

    from windows 7 dee-jay

---
+++ jaydle-project/src/jaydle/Test.java
@@ -0,0 +1,10 @@
+package jaydle;
+import static jaydle.Utilities.*;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	println("hello");	
+	}
+
+}
---
+++ jaydle-project/src/jaydle/WindowsProcessBuilderTest.java
@@ -0,0 +1,22 @@
+package jaydle;
+import static jaydle.Utilities.*;
+import java.io.*;
+
+
+public class WindowsProcessBuilderTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			
+			File youtubeDlExe=new File("F:\\SampleCode");
+			System.out.println(youtubeDlExe.exists());
+
+		}catch(Exception exc)
+		{
+			;
+		}
+
+	}
+
+}
